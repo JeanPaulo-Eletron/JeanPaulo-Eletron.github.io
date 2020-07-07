14c0d481b1d8f8cf962253be8b02b7bd466849cc
JeanPauloRioPreto@GMail.Com <Jean Paulo Athanazio De Mei>
2020-07-06 21:53:22 -0300

    no message

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 var input1   = document.querySelector('#input1')
 if (window.Worker) {
     var myWorker = new Worker('ThreadWork.js');
-	input1.onchange = ()=>{
+	input1.onchange = function(){
         myWorker.postMessage(input1.value)
         console.log('Message posted to worker');
     }
